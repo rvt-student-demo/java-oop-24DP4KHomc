6ba9160318764fdceb8dc934f41c875502d50435
24DP4KHomc <a240428kh@rvt.lv>
2026-01-06 10:29:55 +0200

    Using data.csv

---
+++ src/main/java/rvt/Test.java
@@ -0,0 +1,29 @@
+package rvt;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(Paths.get("data/data.csv"))) {
+            // Ignore header row
+            scanner.nextLine();
+
+            // Read the file until all lines have been read
+            while (scanner.hasNextLine()) {
+                // Read one line
+                String row = scanner.nextLine();
+                String[] parts = row.split(",");
+
+                System.out.println("Name: " + parts[0]);
+                System.out.println("Age: " + parts[1]);
+                System.out.println("Id: " + parts[2]);
+                System.out.println("Email: " + parts[3]);
+                System.out.println(Arrays.toString(parts));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
